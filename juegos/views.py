5fd7220ebab65b24e7e3e579715ea78d47bec318
Daniel Mateu <mateudani97@gmail.com>
2025-08-10 19:42:37 +0200

    Paginacion y filtros en los juegos añadidos

--- juegos/views.py
+++ juegos/views.py
@@ -9,12 +9,46 @@ from .models import Videojuego, Comentario
 from .serializers import VideojuegoSerializer, ComentarioSerializer
 from .permissions import EsAdminOEmpleado
 
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+from .models import Videojuego
+
+
+class VideojuegoFilter(django_filters.FilterSet):
+    # Rango de precio
+    min_precio = django_filters.NumberFilter(field_name='precio', lookup_expr='gte')
+    max_precio = django_filters.NumberFilter(field_name='precio', lookup_expr='lte')
+    # Rango de fecha de lanzamiento
+    min_fecha = django_filters.DateFilter(field_name='fecha_lanzamiento', lookup_expr='gte')
+    max_fecha = django_filters.DateFilter(field_name='fecha_lanzamiento', lookup_expr='lte')
+
+    class Meta:
+        model = Videojuego
+        fields = []
+
+
+# permite ?page_size=XX con un máximo
+class DefaultPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 # === Videojuegos CRUD ===
 class VideojuegoViewSet(ModelViewSet):
     queryset = Videojuego.objects.all()
     serializer_class = VideojuegoSerializer
-    permission_classes = [EsAdminOEmpleado]
+    permission_classes = [EsAdminOEmpleado] 
+
+    #filtros + búsqueda + ordenación + paginación
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_class = VideojuegoFilter
+    search_fields = ['titulo'] 
+    ordering_fields = ['precio', 'fecha_lanzamiento', 'stock', 'titulo']
+    ordering = ['-fecha_lanzamiento']  # orden por defecto
+    pagination_class = DefaultPagination  # para ?page_size=XX (opcional)
+
 
 
 # === Comentarios generales ===
@@ -42,3 +76,6 @@ class CrearComentarioEnVideojuego(APIView):
             serializer.save(videojuego=videojuego, autor=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
--- tienda/settings.py
+++ tienda/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
 ] + APPS_PROPIAS + APPS_TERCEROS
 
 MIDDLEWARE = [
@@ -142,6 +143,15 @@ REST_FRAMEWORK = {
 
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
-    ]
+    ],
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
